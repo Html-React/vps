c529067f0f9296ccf491ae9bdfae96840aa51fcf
KuLiBiN <107316035+Html-React@users.noreply.github.com>
2024-11-09 21:38:51 +0400

    Update install_python3.12.sh

--- install_python3.12.sh
+++ install_python3.12.sh
@@ -25,7 +25,7 @@ if [[ "$INSTALL_PYTHON" =~ ^[yY]$ ]]; then
 	apt update -y
 
 	# Получаем список доступных версий Python
-	AVAILABLE_VERSIONS=$(apt-cache search python3 | grep -oP 'python3\.\d+' | sed 's/python3\.//' | sort -u)
+	AVAILABLE_VERSIONS=$(apt-cache search python3.1 | grep -oP 'python3\.\d+' | sed 's/python3\.//' | sort -u)
 	apt-cache search python3.1
 	# Выводим список доступных версий
 	echo "Доступные версии Python:"
